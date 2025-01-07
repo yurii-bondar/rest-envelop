fd6872d3c6c86ccd7c06c4f40ae26b46aee96e7f
yurii-bondar <bond.in.ua@gmail.com>
2025-01-07 13:22:50 +0200

    docs: added semantic-release configs

--- release.config.js
+++ release.config.js
@@ -2,34 +2,6 @@ module.exports = {
   branches: ['main'],
   plugins: [
     [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'conventionalcommits',
-        releaseRules: [
-          { type: 'feat', release: 'minor' },
-          { type: 'fix', release: 'patch' },
-          { type: 'chore', release: 'patch' },
-          { type: 'docs', release: false },
-          { type: 'style', release: false },
-          { type: 'refactor', release: 'patch' },
-          { type: 'perf', release: 'patch' },
-          { type: 'test', release: false },
-        ],
-        parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
-        },
-      },
-    ],
-    [
-      '@semantic-release/release-notes-generator',
-      {
-        preset: 'conventionalcommits',
-        writerOpts: {
-          commitsSort: ['subject', 'scope'],
-        },
-      },
-    ],
-    [
       '@semantic-release/changelog',
       {
         changelogFile: 'CHANGELOG.md',
@@ -40,16 +12,18 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['CHANGELOG.md', 'package.json'],
-        message: `chore(release): ${nextRelease.version} [skip ci]`,
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
       },
     ],
     [
       '@semantic-release/github',
       {
-        assets: [{
-          path: 'dist/*.js',
-          label: 'Distribution Files',
-        }],
+        assets: [
+          {
+            path: 'dist/*.js',
+            label: 'Distribution Files',
+          },
+        ],
       },
     ],
   ],
